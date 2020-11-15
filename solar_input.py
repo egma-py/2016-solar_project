4478696e5c0e3b807736373e5d9e841004628f1d
Alexey Akimov <akimov.a@phystech.edu>
2020-11-15 23:33:11 +0300

    Fixed modules solar_input and solar_model
    Fixed reading and writing to a file in the solar_input module. Reworked functions that calculate the physical model.

--- solar_input.py
+++ solar_input.py
@@ -2,6 +2,7 @@
 # license: GPLv3
 
 from solar_objects import Star, Planet
+from colors import *
 
 
 def read_space_objects_data_from_file(input_filename):
@@ -19,10 +20,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == 'planet':
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +48,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
-    pass  # FIXME: not done yet
+    star_param = line.split()
+    star.R = float(star_param[1])
+    star.color = star_param[2]
+    star.m = float(star_param[3])
+    star.x = float(star_param[4])
+    star.y = float(star_param[5])
+    star.Vx = float(star_param[6])
+    star.Vy = float(star_param[7])
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,8 +72,14 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
-
+    planet_param = line.split()
+    planet.R = float(planet_param[1])
+    planet.color = planet_param[2]
+    planet.m = float(planet_param[3])
+    planet.x = float(planet_param[4])
+    planet.y = float(planet_param[5])
+    planet.Vx = float(planet_param[6])
+    planet.Vy = float(planet_param[7])
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
@@ -77,10 +94,19 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
-
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+            obj_param_spisok = [0, 0, 0, 0, 0, 0, 0, 0]
+            obj_param_spisok[0] = obj.type
+            obj_param_spisok[1] = str(round(obj.R, 3))
+            obj_param_spisok[2] = obj.color
+            obj_param_spisok[3] = str(round(obj.m, 3))
+            obj_param_spisok[4] = str(round(obj.x, 3))
+            obj_param_spisok[5] = str(round(obj.y, 3))
+            obj_param_spisok[6] = str(round(obj.Vx, 3))
+            obj_param_spisok[7] = str(round(obj.Vy, 3))
+            obj_param = ' '.join(obj_param_spisok)
+            print(out_file, obj_param)
+            out_file.write(obj_param + "\n")
+        out_file.close()
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
